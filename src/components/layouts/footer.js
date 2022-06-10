a9ca2655def5698d8dc74c06e88a409a2def487a
Julien <jujufrancois1999@gmail.com>
2022-06-10 16:12:39 +0200

    Dekstop css footer

--- src/components/layouts/footer.js
+++ src/components/layouts/footer.js
@@ -19,18 +19,22 @@ const Footer = () => {
           </Link>
         </div>
         <div className="sitemap-container-footer">
-          <Link to="">Expérience utilisateur</Link>
-          <Link to="">Culture green</Link>
+          <Link to="" className="link-margin-right">
+            Expérience utilisateur
+          </Link>
+          <Link to="" className="link-margin-right">
+            Culture green
+          </Link>
           <Link to="">Impact écologique</Link>
         </div>
         <div className="socialmedia-footer">
-          <Link to="">
+          <Link to="" className="link-margin-right">
             <img src={twitter} alt="twitter" />
           </Link>
-          <Link to="">
+          <Link to="" className="link-margin-right">
             <img src={linkedin} alt="linekdin" />
           </Link>
-          <Link to="">
+          <Link to="" className="link-margin-right">
             <img src={facebook} alt="facebook" />
           </Link>
           <Link to="">
@@ -40,8 +44,12 @@ const Footer = () => {
       </div>
       <div className="bottom-footer">
         <div className="politique-footer">
-          <Link to="">Conditions d'utilisation</Link>
-          <Link to="">Politique de confidentialité</Link>
+          <Link to="" className="link-margin-right">
+            Conditions d'utilisation
+          </Link>
+          <Link to="" className="link-margin-right">
+            Politique de confidentialité
+          </Link>
           <Link to="">Politique de sécurité</Link>
         </div>
         <div className="copyright-footer">
